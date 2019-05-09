6eff221073e268dd83bf12106e97ccb3a96d6e14
Nabil Fannane <nabil@goadroit.com>
2019-05-09 11:08:50 -0700

    last commit

--- main.js
+++ main.js
@@ -1,67 +1,68 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
-
-document.getElementById("cart-info").addEventListener("click", function() {
+  document.getElementById("cart-info").addEventListener("click", function () {
     const cart = document.getElementById("cart");
     cart.classList.toggle("show-cart");
-    
-  });
-  const cart_info=document.querySelector('.cart-info');
-  const cart=document.querySelector('.cart');
-  let val=true;
-  cart_info.addEventListener('click',()=>{
 
-     if (val){
-       cart.style.opacity="1";
-       cart.classList.add("rotate");
-        val=false;}
-      else {cart.style.display="none"}
+  });
+  const cart_info = document.querySelector('.cart-info');
+  const cart = document.querySelector('.cart');
+  let val = true;
+  cart_info.addEventListener('click', () => {
+
+    if (val) {
+      cart.style.opacity = "1";
+      cart.classList.add("rotate");
+      val = false;
+    } else {
+      cart.style.display = "none"
+    }
   })
 
- 
 
-  $(window).scroll(function(){
-    if($(document).scrollTop() > 100){
-        $('.navbar').addClass('shrink');
-        
-     
+
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 100) {
+      $('.navbar').addClass('shrink');
+
+
     } else {
-        $('.navbar').removeClass('shrink');
+      $('.navbar').removeClass('shrink');
     }
-});
- 
+  });
 
-  
-window.sr = ScrollReveal();
+
+
+  window.sr = ScrollReveal();
 
 
   sr.reveal('#services div', {
-    duration: 1000,
-    origin:'bottom',
-    distance:'300px'
-  
+    duration: 700,
+    origin: 'bottom',
+    distance: '300px'
+
   });
 
   sr.reveal('.footer1,#row1', {
-    duration: 1000,
-    origin:'left',
-    distance:'300px'
-  
+    duration: 900,
+    origin: 'left',
+    distance: '300px'
+
   });
   sr.reveal('.footer2, #row2', {
-    duration: 1000,
-    origin:'right',
-    distance:'300px'
-  
+    duration: 900,
+    origin: 'right',
+    distance: '300px'
+
   });
 
   sr.reveal('#orders div,nav,#one .container', {
     duration: 1000,
-    origin:'top',
-    distance:'300px'
-  
+    origin: 'top',
+    distance: '300px'
+
   });
 
-  
 
-  })
+
+})
